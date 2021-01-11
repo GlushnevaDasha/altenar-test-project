87709c09b416e26f77620a6abf76388e97af372a
Dasha Glushneva <glushnevadasha@mail.ru>
2021-01-11 11:40:53 +0300

    add mobile style product

--- src/page/Product/index.js
+++ src/page/Product/index.js
@@ -1,4 +1,5 @@
 import React, { useState, useContext, useEffect } from "react";
+import FastAverageColor from "fast-average-color";
 import { ThemeContext, BasketContext } from "../../utils/context";
 import { getParameterFromUrl } from "../../utils/functions";
 import { getSearchByID } from "../../utils/api";
@@ -9,13 +10,31 @@ import AddShoppingCartSharpIcon from "@material-ui/icons/AddShoppingCartSharp";
 import BrightnessHighIcon from "@material-ui/icons/BrightnessHigh";
 import BrightnessLowIcon from "@material-ui/icons/BrightnessLow";
 
+import "../../utils/styles/page/product.css";
+
 export default function Product() {
   const theme = useContext(ThemeContext);
   const basket = useContext(BasketContext);
   const [product, setProduct] = useState([]);
-
+  const [color, setColor] = useState({});
   useEffect(() => {
     getData();
+    if (product.length !== 0) {
+      const fac = new FastAverageColor();
+
+      fac
+        .getColorAsync(product[0].artworkUrl100)
+        .then(colors => {
+          console.log("Average color", colors);
+          setColor(colors);
+        })
+        .catch(e => {
+          console.log(e);
+        });
+    }
+
+    // container.style.backgroundColor = color.rgba;
+    // container.style.color = color.isDark ? "#fff" : "#000";
   }, [product.length]);
 
   const [isFeath, setFeath] = useState(false);
@@ -34,102 +53,190 @@ export default function Product() {
   }
 
   return (
-    <div
-      className={theme.theme ? "white" : "dark"}
-      style={{
-        padding: "5vh 5vw ",
-        height: "100vh"
-      }}
-    >
-      {isFeath ? (
-        <div>
-          <div
-            style={{
-              display: "flex",
-              justifyContent: "space-between"
-              // height: "100vh",
-              // padding: "5vh 5vw "
-            }}
-          >
-            <div
-              style={{
-                backgroundImage: `url(${product[0].artworkUrl100.replace(
-                  "100x100bb",
-                  "450x450bb"
-                )})`,
-                width: 450,
-                height: 450
-              }}
-            />
-            <div>
+    <div>
+      <div className={theme.theme ? "white full-page" : "dark full-page"}>
+        {isFeath ? (
+          <div>
+            <div className='center'>
               <div
+                className={
+                  product[0].kind === "feature-movie"
+                    ? "poster-video"
+                    : "poster-sound"
+                }
                 style={{
-                  display: "flex",
-                  alignItems: "center",
-                  justifyContent: "space-between"
+                  backgroundImage: `url(${product[0].artworkUrl100.replace(
+                    "100x100bb",
+                    "450x450bb"
+                  )})`
                 }}
-              >
-                <Typography component='h6' variant='h6' className='text'>
-                  {product[0].trackName}
+              />
+              <div>
+                <div className='name-container'>
+                  <Typography component='h6' variant='h6' className='text'>
+                    {product[0].trackName}
+                  </Typography>
+                  <IconButton
+                    aria-label='menu'
+                    onClick={() => {
+                      theme.saveTheme(!theme.theme);
+                    }}
+                  >
+                    {theme.theme ? (
+                      <BrightnessLowIcon />
+                    ) : (
+                      <BrightnessHighIcon />
+                    )}
+                  </IconButton>
+                </div>
+                <Typography
+                  variant='subtitle1'
+                  color='textSecondary'
+                  className='text'
+                >
+                  {product[0].artistName}
                 </Typography>
-                <IconButton
-                  aria-label='menu'
-                  onClick={() => {
-                    theme.saveTheme(!theme.theme);
-                  }}
-                >
-                  {theme.theme ? <BrightnessLowIcon /> : <BrightnessHighIcon />}
-                </IconButton>
-              </div>
-              {/* <Typography
-                variant='subtitle1'
-                color='textSecondary'
-                className='text'
-              >
-                {product[0].artistName}
-              </Typography> */}
-              <div style={{ display: "flex", alignItems: "center" }}>
-                <Typography variant='subtitle1' color='textSecondary'>
-                  {product[0].trackPrice > 0
-                    ? product[0].trackPrice
-                    : product[0].collectionPrice || product[0].collectionPrice}
-                  {" " + product[0].currency}
-                </Typography>{" "}
-                <IconButton
-                  aria-label='previous'
-                  onClick={() => {
-                    basket.saveBasket(product[0]);
-                  }}
-                >
-                  <AddShoppingCartSharpIcon />
-                </IconButton>
-              </div>
+                <div style={{ display: "flex", alignItems: "center" }}>
+                  <Typography variant='subtitle1' color='textSecondary'>
+                    {product[0].trackPrice > 0
+                      ? product[0].trackPrice
+                      : product[0].collectionPrice ||
+                        product[0].collectionPrice}
+                    {" " + product[0].currency}
+                  </Typography>{" "}
+                  <IconButton
+                    aria-label='previous'
+                    onClick={() => {
+                      basket.saveBasket(product[0]);
+                    }}
+                  >
+                    <AddShoppingCartSharpIcon />
+                  </IconButton>
+                </div>
 
-              <video controls autoplay name='media' style={{ width: 500 }}>
-                <source
-                  src={`${product[0].previewUrl}`}
-                  type={
-                    product[0].kind === "feature-movie"
-                      ? "video/x-m4v"
-                      : "audio/x-m4a"
-                  }
-                />
-              </video>
+                <video controls name='media' className='video-container'>
+                  <source
+                    src={`${product[0].previewUrl}`}
+                    type={
+                      product[0].kind === "feature-movie"
+                        ? "video/x-m4v"
+                        : "audio/x-m4a"
+                    }
+                  />
+                </video>
+              </div>
+            </div>
+            <div>
+              <Typography variant='subtitle1' color='textSecondary'>
+                {product[0].longDescription}
+              </Typography>
             </div>
           </div>
+        ) : null}
+      </div>
 
-          <div>
-            {" "}
-            <Typography
-              variant='subtitle1'
-              color='textSecondary'
-              // className='text'
-            >
-              {product[0].longDescription}
-            </Typography>
+      <div className='mobile-page'>
+        {isFeath ? (
+          <div id='cont'>
+            <div className='center'>
+              <div
+                id='poster'
+                className={
+                  product[0].kind === "feature-movie"
+                    ? "poster-video"
+                    : "poster-sound"
+                }
+                style={{
+                  backgroundImage: `url(${product[0].artworkUrl100.replace(
+                    "100x100bb",
+                    "450x450bb"
+                  )})`
+                }}
+              >
+                <div
+                  style={{
+                    // height: "100%",
+                    display: "flex",
+                    flexDirection: "column",
+                    paddingTop:
+                      product[0].kind === "feature-movie" ? "70vw" : "60vw",
+                    paddingBottom:
+                      product[0].kind === "feature-movie" ? "0vw" : "35vw",
+                    paddingLeft: 10,
+                    paddingRight: 10,
+
+                    background: `linear-gradient(rgba(0, 0, 0, 0) 25% ,${color.rgba} 50%, ${color.rgba} 100% )`
+                  }}
+                >
+                  <video controls name='media' className='video-container'>
+                    <source
+                      src={`${product[0].previewUrl}`}
+                      type={
+                        product[0].kind === "feature-movie"
+                          ? "video/x-m4v"
+                          : "audio/x-m4a"
+                      }
+                    />
+                  </video>
+
+                  <div className='name-container'>
+                    <Typography
+                      component='h6'
+                      variant='h6'
+                      className={color.isLight ? "" : "dark-text"}
+                    >
+                      {product[0].trackName}
+                    </Typography>
+                  </div>
+                  <Typography
+                    variant='subtitle1'
+                    color='textSecondary'
+                    className={color.isLight ? "text" : "dark-text text"}
+                  >
+                    {product[0].artistName}
+                  </Typography>
+                  <div
+                    style={{
+                      display: "flex",
+                      alignItems: "center",
+                      justifyContent: "space-between"
+                    }}
+                  >
+                    <Typography
+                      variant='subtitle1'
+                      color='textSecondary'
+                      className={color.isLight ? "text" : "dark-text text"}
+                    >
+                      {product[0].trackPrice > 0
+                        ? product[0].trackPrice
+                        : product[0].collectionPrice ||
+                          product[0].collectionPrice}
+                      {" " + product[0].currency}
+                    </Typography>
+                    <IconButton
+                      aria-label='previous'
+                      className={color.isLight ? "" : "dark-text text"}
+                      onClick={() => {
+                        basket.saveBasket(product[0]);
+                      }}
+                    >
+                      <AddShoppingCartSharpIcon />
+                    </IconButton>
+                  </div>
+
+                  <Typography
+                    variant='subtitle1'
+                    color='textSecondary'
+                    className={color.isLight ? "" : "dark-text"}
+                  >
+                    {product[0].longDescription}
+                  </Typography>
+                </div>
+              </div>
+            </div>
           </div>
-        </div>
-      ) : null}
+        ) : null}
+      </div>
     </div>
   );
 }
